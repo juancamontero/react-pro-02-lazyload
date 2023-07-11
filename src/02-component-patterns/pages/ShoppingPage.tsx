6ba7a37ed6e98e1975db9046069bd7ced08ac750
Juan Montero <juanca.montero76@gmail.com>
2023-07-11 15:23:01 -0500

    pre npm ready

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -6,7 +6,6 @@ import {
 } from "../components";
 import { products } from "../data/products";
 
-import "../styles/custom-styles.css";
 
 const product = products[0];
 
@@ -25,33 +24,16 @@ export const ShoppingPage = () => {
         <ProductCard
           key={product.id}
           product={product}
-          className="bg-dark"
           initialValues={{
             count: 4,
             maxCount: 10,
           }}
         >
-          {({ reset, count, isMaxReached, onIncreaseBy , maxCount}) => (
+          {({ reset, count, isMaxReached, onIncreaseBy, maxCount }) => (
             <>
-              <ProductImage className="custom-image" />
-              <ProductTitle className="text-white text-bold" />
-              <ProductButtons className="custom-btns" />
-
-              <button onClick={reset}>RESET</button>
-              <hr />
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-around",
-                }}
-              >
-                <button onClick={() => onIncreaseBy(-2)}>-2</button>
-                <span>{count} -max- {maxCount}</span>
-                {!isMaxReached && (
-                  <button onClick={() => onIncreaseBy(2)}>+2</button>
-                )}
-              </div>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
             </>
           )}
         </ProductCard>
